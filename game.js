d1e9e55bae84f6495da0d99f2ffb99fb0e132054
schalchlincode <schalchlincode@gmail.com>
2025-02-22 01:23:49 -0600

    Adjust framewidth to 80 and framehight 110. Fix walk left and walk right animation

--- game.js
+++ game.js
@@ -19,15 +19,65 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.spritesheet("player", "assets/player_spritesheet.png", {
-    frameWidth: 70, // Adjust this based on your sprite size
+    frameWidth: 80, // Adjust this based on your sprite size
     frameHeight: 110,
   });
 }
 
 function create() {
-  this.player = this.add.sprite(400, 300, "player", 0); // Display player
+  // Add player sprite at (400, 300) using the first frame of the spritesheet
+  this.player = this.physics.add.sprite(400, 300, "player", 0);
+  this.player.setCollideWorldBounds(true); // Prevents leaving the screen
+
+  // Define animations (adjust frame numbers if needed)
+  this.anims.create({
+    key: "walk-down",
+    frames: this.anims.generateFrameNumbers("player", { start: 18, end: 23 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "walk-left",
+    frames: this.anims.generateFrameNumbers("player", { start: 9, end: 10 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "walk-right",
+    frames: this.anims.generateFrameNumbers("player", { start: 9, end: 10 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "walk-up",
+    frames: this.anims.generateFrameNumbers("player", { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  // Enable keyboard input
+  this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  console.log("Game updating...");
+  this.player.setVelocity(0); // Stop movement when no keys are pressed
+
+  if (this.cursors.left.isDown) {
+    this.player.setVelocityX(-200);
+    this.player.anims.play("walk-left", true);
+  } else if (this.cursors.right.isDown) {
+    this.player.setVelocityX(200);
+    this.player.anims.play("walk-right", true);
+  } else if (this.cursors.up.isDown) {
+    this.player.setVelocityY(-200);
+    this.player.anims.play("walk-up", true);
+  } else if (this.cursors.down.isDown) {
+    this.player.setVelocityY(200);
+    this.player.anims.play("walk-down", true);
+  } else {
+    this.player.anims.stop(); // Stop animation when idle
+  }
 }
