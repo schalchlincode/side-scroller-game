e8107e73e4168dd9d4091749fe94efff73c63585
schalchlincode <schalchlincode@gmail.com>
2025-03-22 12:34:56 -0500

    Refactor SideScrollerScene to match MainScene structure with constants, inputs, and entity management

--- src/scenes/sideScrollerScene.js
+++ src/scenes/sideScrollerScene.js
@@ -1,85 +1,139 @@
 import * as Phaser from "phaser";
 
 class SideScrollerScene extends Phaser.Scene {
+  CONSTANTS = {
+    KEYS: {
+      SPRITES: {
+        PLAYER: "player",
+        JUMP: "cat_jump",
+      },
+      ANIMATIONS: {
+        WALK: "walk",
+        JUMP: "cat_jump",
+      },
+    },
+  };
+
+  inputs = {
+    cursors: undefined,
+    jump: undefined,
+  };
+
+  entities = {
+    player: undefined,
+    ground: undefined,
+  };
+
   constructor() {
     super({ key: "SideScrollerScene" });
   }
 
   preload() {
-    this.load.spritesheet("player", "src/assets/images/cat_run.png", {
-      frameWidth: 64,
-      frameHeight: 64,
-    });
+    this.load.spritesheet(
+      this.CONSTANTS.KEYS.SPRITES.PLAYER,
+      "src/assets/images/cat_run.png",
+      { frameWidth: 64, frameHeight: 64 }
+    );
 
-    this.load.spritesheet("cat_jump", "src/assets/images/cat_jump.png", {
-      frameWidth: 64,
-      frameHeight: 64,
-    });
+    this.load.spritesheet(
+      this.CONSTANTS.KEYS.SPRITES.JUMP,
+      "src/assets/images/cat_jump.png",
+      { frameWidth: 64, frameHeight: 64 }
+    );
   }
 
   create() {
-    // Ground platform
-    const ground = this.physics.add.staticGroup();
-    ground.create(400, 580, null).setDisplaySize(800, 40).refreshBody();
+    this.#createGround();
+    this.#createPlayer();
+    this.#createAnimations();
+    this.#createInputs();
+    this.#createCamera();
+  }
 
-    // Player
-    this.player = this.physics.add.sprite(100, 450, "player", 0);
-    this.player.setCollideWorldBounds(true);
-    this.player.setBounce(0.1);
+  update() {
+    const player = this.entities.player;
+    const onGround = player.body.touching.down;
 
+    if (onGround) {
+      player.setVelocityX(0);
+    }
+
+    if (!onGround) {
+      player.anims.play(this.CONSTANTS.KEYS.ANIMATIONS.JUMP, true);
+      return;
+    }
+
+    if (this.inputs.cursors.left.isDown) {
+      player.setVelocityX(-200);
+      player.anims.play(this.CONSTANTS.KEYS.ANIMATIONS.WALK, true);
+      player.setFlipX(true);
+    } else if (this.inputs.cursors.right.isDown) {
+      player.setVelocityX(200);
+      player.anims.play(this.CONSTANTS.KEYS.ANIMATIONS.WALK, true);
+      player.setFlipX(false);
+    } else {
+      player.anims.stop();
+    }
+
+    if (this.inputs.jump.isDown && onGround) {
+      player.setVelocityY(-400);
+      player.anims.play(this.CONSTANTS.KEYS.ANIMATIONS.JUMP, true);
+    }
+  }
+
+  #createGround() {
+    this.entities.ground = this.physics.add.staticGroup();
+    this.entities.ground
+      .create(400, 580, null)
+      .setDisplaySize(800, 40)
+      .refreshBody();
+  }
+
+  #createPlayer() {
+    this.entities.player = this.physics.add.sprite(
+      100,
+      450,
+      this.CONSTANTS.KEYS.SPRITES.PLAYER,
+      0
+    );
+    this.entities.player.setCollideWorldBounds(true);
+    this.entities.player.setBounce(0.1);
+
+    this.physics.add.collider(this.entities.player, this.entities.ground);
+  }
+
+  #createAnimations() {
     this.anims.create({
-      key: "walk",
-      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 5 }),
+      key: this.CONSTANTS.KEYS.ANIMATIONS.WALK,
+      frames: this.anims.generateFrameNumbers(
+        this.CONSTANTS.KEYS.SPRITES.PLAYER,
+        { start: 0, end: 5 }
+      ),
       frameRate: 10,
       repeat: -1,
     });
 
     this.anims.create({
-      key: "cat_jump",
-      frames: this.anims.generateFrameNumbers("cat_jump", { start: 0, end: 6 }),
+      key: this.CONSTANTS.KEYS.ANIMATIONS.JUMP,
+      frames: this.anims.generateFrameNumbers(
+        this.CONSTANTS.KEYS.SPRITES.JUMP,
+        { start: 0, end: 6 }
+      ),
       frameRate: 6,
       repeat: -1,
     });
-
-    this.physics.add.collider(this.player, ground);
-
-    // Input
-    this.cursors = this.input.keyboard.createCursorKeys();
-    this.spacebar = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.SPACE
-    );
-
-    // Camera follows player
-    this.cameras.main.startFollow(this.player);
-    this.cameras.main.setBounds(0, 0, 2000, 600);
   }
 
-  update() {
-    if (this.player.body.touching.down) {
-      this.player.setVelocityX(0);
-    }
+  #createInputs() {
+    this.inputs.cursors = this.input.keyboard.createCursorKeys();
+    this.inputs.jump = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+  }
 
-    if (!this.player.body.touching.down) {
-      this.player.anims.play("cat_jump", true);
-      return;
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-200);
-      this.player.anims.play("walk", true);
-      this.player.setFlipX(true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(200);
-      this.player.anims.play("walk", true);
-      this.player.setFlipX(false);
-    } else {
-      this.player.anims.stop();
-    }
-
-    if (this.spacebar.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-400);
-      this.player.anims.play("cat_jump", true); // ✅ NEW: play the jump animation
-    }
+  #createCamera() {
+    this.cameras.main.startFollow(this.entities.player);
+    this.cameras.main.setBounds(0, 0, 2000, 600);
   }
 }
 
